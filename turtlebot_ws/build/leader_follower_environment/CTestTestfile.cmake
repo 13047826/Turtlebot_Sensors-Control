3017c9cc9e75e2ffd36fd1dcf925a7ae9587ab73
13047826 <13047826@student.uts.edu.au>
2023-09-14 22:05:47 +1000

    turtlebot_ws with modified turtlebot3_burger_urdf.xacro and additional camera.xacro inside turtlebot_ws/src/turtlebot3/turtlebot3_description/urdf

---
+++ turtlebot_ws/build/leader_follower_environment/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/girin/turtlebot_ws/src/leader_follower_environment
+# Build directory: /home/girin/turtlebot_ws/build/leader_follower_environment
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
