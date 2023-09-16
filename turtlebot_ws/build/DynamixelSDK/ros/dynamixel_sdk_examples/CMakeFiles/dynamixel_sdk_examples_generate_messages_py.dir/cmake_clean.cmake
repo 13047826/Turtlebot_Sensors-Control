034723760a45ad34ecc37e28517933580aeab738
13047826 <13047826@student.uts.edu.au>
2023-09-16 15:06:06 +1000

    removed .git and .github packages

---
+++ turtlebot_ws/build/DynamixelSDK/ros/dynamixel_sdk_examples/CMakeFiles/dynamixel_sdk_examples_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/msg/_BulkSetItem.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/msg/_SetPosition.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/msg/_SyncSetPosition.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/msg/__init__.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/srv/_BulkGetItem.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/srv/_GetPosition.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/srv/_SyncGetPosition.py"
+  "/home/girin/turtlebot_ws/devel/lib/python3/dist-packages/dynamixel_sdk_examples/srv/__init__.py"
+  "CMakeFiles/dynamixel_sdk_examples_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dynamixel_sdk_examples_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ turtlebot_ws/devel/share/gennodejs/ros/dynamixel_sdk_examples/srv/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let BulkGetItem = require('./BulkGetItem.js')
+let SyncGetPosition = require('./SyncGetPosition.js')
+let GetPosition = require('./GetPosition.js')
+
+module.exports = {
+  BulkGetItem: BulkGetItem,
+  SyncGetPosition: SyncGetPosition,
+  GetPosition: GetPosition,
+};
