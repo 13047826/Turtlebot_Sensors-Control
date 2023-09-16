c0e043a68b94ae346c3100279c5f64a9ef9ed5d4
13047826 <13047826@student.uts.edu.au>
2023-09-16 15:03:06 +1000

    attempting to fix inaccessible folder

--- turtlebot_ws/build/DynamixelSDK/ros/dynamixel_sdk/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/girin/turtlebot_ws/src/DynamixelSDK/ros/dynamixel_sdk/include".split(';') if "/home/girin/turtlebot_ws/src/DynamixelSDK/ros/dynamixel_sdk/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ldynamixel_sdk".split(';') if "-ldynamixel_sdk" != "" else []
-PROJECT_NAME = "dynamixel_sdk"
-PROJECT_SPACE_DIR = "/home/girin/turtlebot_ws/devel"
-PROJECT_VERSION = "3.7.51"
--- turtlebot_ws/build/leader_follower_environment/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/girin/turtlebot_ws/src/leader_follower_environment
-# Build directory: /home/girin/turtlebot_ws/build/leader_follower_environment
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
